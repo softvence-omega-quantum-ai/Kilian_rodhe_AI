795e5e5420e1f9023dd7bd1ba8ed83b8147b3e1a
Asifuzzaman_Asif <asifuzzamanasif734@gmail.com>
2026-02-24 14:25:51 +0600

    transfered future stack to quantum ai

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
+# from fastapi import FastAPI
+# from fastapi.middleware.cors import CORSMiddleware
+# import uvicorn as uv
+
+# from app.api.v1.endpoints import t_shirt_endpoint
+
+# app = FastAPI()
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
+
+# app.include_router(t_shirt_endpoint.router)
+
+# @app.get("/")
+# def read_root():    
+#     return {"message": "Welcome to the Kilian Rodhe API.Image Generation Service and project name is: Kilian_Rodhe"}
+
+
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn as uv
@@ -5,9 +27,13 @@ import uvicorn as uv
 from app.api.v1.endpoints import t_shirt_endpoint
 
 app = FastAPI()
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -17,4 +43,6 @@ app.include_router(t_shirt_endpoint.router)
 
 @app.get("/")
 def read_root():    
-    return {"message": "Welcome to the Kilian Rodhe API.Image Generation Service and project name is: Kilian_Rodhe"}
+    return {
+        "message": "Welcome to the Kilian Rodhe API. Image Generation Service and project name is: Kilian_Rodhe"
+    }
